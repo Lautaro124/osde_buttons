7ef39a2df372be08c300f5da18c4d6d4ff6597bc
Lautaro Gonzalez <lautarogonzalez@CTL-DESA-2.fibertel.com.ar>
2022-06-16 12:52:59 -0300

    changin info

---
+++ lib/assets/enums/urls.dart
@@ -0,0 +1,7 @@
+enum Url {
+  base('https://osded.cloud.invgate.net/api/v1/'),
+  breakNews('breakingnews.all');
+
+  const Url(this.value);
+  final String value;
+}
--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,19 +1,20 @@
 import 'package:dio/dio.dart';
 
-String baseUrl = 'https://osded.cloud.invgate.net/api/v1';
+import '../assets/enums/urls.dart';
 
 class RequesApi {
   final String route;
   RequesApi({required this.route});
 
+  String _url() => Url.base.value + route;
+
   Future<Response> get() async {
-    String url = baseUrl + route;
     Options options = Options(headers: {
       'Authorization':
           'Basic c29wb3J0ZXVzdWFyaW9zOkZFdWhSc2lPQThpU2doNWhXODVOQ3RYYw==',
       'Cookie': 'PHPSESSID=fd29affdc28a8e89c49919b51db1a50d'
     });
 
-    return await Dio().get(url, options: options);
+    return await Dio().get(_url(), options: options);
   }
 }
