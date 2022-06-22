c85005c6b039e0d28d6dd27943892e1a5f8f3691
Lautaro Gonzalez <lautarogonzalez@CTL-DESA-2.fibertel.com.ar>
2022-06-22 12:10:00 -0300

    Google adding

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:osde_botonera/assets/enums/navigation_route.dart';
+import 'package:osde_botonera/screens/login/utils/google_sign_in.dart';
 
 import '../../widget/generator_screen/screen_generator.dart';
 
@@ -19,14 +21,30 @@ class _LoginState extends State<Login> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Text('Bienvenido'),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, NavigationRoute.dashboard.route);
-            },
-            child: const Text('Osde'),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.mail_outlined),
+            label: const Text('Iniciar secion con google'),
+            onPressed: signIn,
           )
         ],
       ),
     );
+  }
+
+  Future<dynamic> signIn() async {
+    final GoogleSignInAccount? userAcount = await GoogleSignInApi.login();
+
+    redirectDashboard(userAcount!);
+  }
+
+  void redirectDashboard(GoogleSignInAccount user) {
+    List<String> mailsAcepted = ['@ctl', '@osde'];
+
+    for (String value in mailsAcepted) {
+      if (user.email.contains(value)) {
+        Navigator.pushNamed(context, NavigationRoute.dashboard.route);
+        break;
+      }
+    }
   }
 }
